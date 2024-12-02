028b883785e303aa1def9df49862b97be81f471e
enrico <enrico.dsf@gmail.com>
2024-12-02 22:21:43 +0100

    day 2 solution

---
+++ day02/solution.py
@@ -0,0 +1,23 @@
+from itertools import pairwise
+
+def is_valid(levels):
+    diff = set(y - x for x, y in pairwise(levels))
+    return (diff <= {1, 2, 3}) or (diff <= {-1, -2, -3})
+
+
+def is_tolerated(levels):
+    for i in range(len(levels)):
+        if is_valid(levels[:i] + levels[i+1:]):
+            return True
+    return False
+
+
+with open("day02/data") as f:
+    reports = [[int(x) for x in l.split()] for l in f.readlines()]
+
+
+# ==== PART 1 ====
+print(sum(is_valid(r) for r in reports))
+
+# ==== PART 2 ====
+print(sum(is_valid(r) or is_tolerated(r) for r in reports))
