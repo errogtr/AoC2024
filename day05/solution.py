79b9a0e265042ab7456c940c20870ae51dbc95d5
enrico <enrico.dsf@gmail.com>
2024-12-05 09:22:06 +0100

    minor refactor

--- day05/solution.py
+++ day05/solution.py
@@ -1,5 +1,10 @@
 from collections import defaultdict
-from itertools import combinations, pairwise, product
+from itertools import combinations
+
+
+def get_middle(update):
+    pages = update.split(",")
+    return int(pages[len(pages)//2])
 
 
 with open("day05/data") as f:
@@ -10,26 +15,18 @@ for r in rules.split():
     a, b = r.split("|")
     rules_dict[a].add(b)
 
-
-# ==== PART 1 ====
-middle = 0
-for update in updates.split():
-    pages = update.split(",")
-    for p, q in pairwise(pages):
-        if q not in rules_dict[p]:
-            break
-    else:
-        middle += int(pages[len(pages)//2])
-print(middle)
-
-
-# ==== PART 2 ====
-middle = -middle
+ordered = list()
 for update in updates.split():
     pages = update.split(",")
     for i, j in combinations(range(len(pages)), 2):
         p, q = pages[i], pages[j]
         if q not in rules_dict[p]:
             pages[i], pages[j] = pages[j], pages[i]
-    middle += int(pages[len(pages)//2])
-print(middle)
+    ordered.append(",".join(pages))
+
+
+# ==== PART 1 ====
+print(sum(get_middle(o) for u, o in zip(updates.split(), ordered) if u == o))
+
+# ==== PART 2 ====
+print(sum(get_middle(o) for u, o in zip(updates.split(), ordered) if u != o))
