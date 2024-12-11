2022b37a3b85e10d321c87bfdfca8658fb65a0cb
enrico <enrico.dsf@gmail.com>
2024-12-12 00:30:39 +0100

    day 11 solution

---
+++ day11/solution.py
@@ -0,0 +1,36 @@
+from collections import Counter
+from math import floor, log
+
+
+def count_stones(stones):
+    blink = Counter()
+    for stone, count in stones.items():
+        if stone == 0:
+            blink[1] += count
+        else:
+            digits = floor(log(stone, 10)) + 1
+            if digits % 2:  # number of digits is odd
+                blink[2024 * stone] += count
+            else:  # number of digits is even
+                m = 10**(digits // 2)
+                blink[stone // m] += count
+                blink[stone % m] += count
+    return blink       
+
+
+with open("day11/data") as f:
+    stones = [int(x) for x in f.read().split()]
+
+
+stones = Counter(stones)
+
+# ==== PART 1 ====
+for _ in range(25):
+    stones = count_stones(stones)
+print(stones.total())
+
+
+# ==== PART 2 ====
+for _ in range(50):
+    stones = count_stones(stones)
+print(stones.total())
