fbce0e9f432ed012e98f43c8528ee2c6c424b533
enrico <enrico.dsf@gmail.com>
2024-12-13 08:29:55 +0100

    day12 solution

--- day12/solution.py
+++ day12/solution.py
@@ -1,49 +1,82 @@
 from collections import defaultdict
 
+RIGHT, DOWN, LEFT, UP = (1, 0), (0, 1), (-1, 0), (0, -1)
+DIRS = [RIGHT, DOWN, LEFT, UP]
 
-DIRS = [(1, 0), (0, -1), (-1, 0), (0, 1)]
+
+def get_nn(x, y, plant_xy, garden):
+    nn_xy = list()
+    for dx, dy in DIRS:
+        nn_x, nn_y = x + dx, y + dy
+        if garden.get((nn_x, nn_y)) == plant_xy:
+            nn_xy.append((nn_x, nn_y))
+    return nn_xy
+
+
+def in_line(pts):
+    x_coords, y_coords = zip(*pts)
+    return len(set(x_coords)) == 1 or len(set(y_coords)) == 1
 
 
 garden = dict()
-with open("day12/data") as f:
+with open("day12/example") as f:
     for y, row in enumerate(f.read().splitlines()):
         for x, plant in enumerate(row):
             garden[(x, y)] = plant
 
 
-
-regions = list()  # [(x, y, num_nn)]
-visited = set()
+price, regions = 0, list()
+visited = set()  # [(x, y, num_nn)]
 for (x, y), plant_xy in garden.items():
     if (x, y) in visited:
         continue
-    curr_xy = [(x, y)]
-    region = region()
+    curr_xy = {(x, y)}
+    region = defaultdict(int)
     while curr_xy:
         curr_x, curr_y = curr_xy.pop()
-        region[(curr_x, curr_y)] = 4
+        region[(curr_x, curr_y)] = 0
         visited.add((curr_x, curr_y))
-
-        nn_curr_xy = list()
-        for dx, dy in DIRS:
-            nn_x, nn_y = curr_x + dx, curr_y + dy
-            if garden.get((nn_x, nn_y)) == plant_xy:
-                nn_curr_xy.append((nn_x, nn_y))
-
-
-        for nn_x, nn_y in nn_curr_xy:
-            region[(curr_x, curr_y)] -= 1
+        nn_xy = get_nn(curr_x, curr_y, plant_xy, garden)
+        for nn_x, nn_y in nn_xy:
+            region[(curr_x, curr_y)] += 1
             if (nn_x, nn_y) not in visited:
-                curr_xy.append((nn_x, nn_y))
+                curr_xy.add((nn_x, nn_y))
 
+    price += len(region) * sum(4 - i for i in region.values())
     regions.append(region)
 
+print(price)
 
-value = 0
+
+# ==== PART 2 ====
+price = 0
 for region in regions:
-    area = len(region)
-    perimeter = sum(region.values())
-    value += area * perimeter
+    edges = 0
+    for (x, y) in region:
+        # top left
+        if (x, y - 1) not in region and (x - 1, y) not in region:
+            edges += 1
+        elif (x, y - 1) in region and (x - 1, y) in region and (x - 1, y - 1) not in region:
+            edges += 1
 
-print(value)
+        # top right
+        if (x, y - 1) not in region and (x + 1, y) not in region:
+            edges += 1
+        elif (x, y - 1) in region and (x + 1, y) in region and (x + 1, y - 1) not in region:
+            edges += 1
+
+        # bottom right
+        if (x, y + 1) not in region and (x + 1, y) not in region:
+            edges += 1
+        elif (x, y + 1) in region and (x + 1, y) in region and (x + 1, y + 1) not in region:
+            edges += 1
+
+        # bottom left
+        if (x - 1, y) not in region and (x, y + 1) not in region:
+            edges += 1
+        elif (x - 1, y) in region and (x, y + 1) in region and (x - 1, y + 1) not in region:
+            edges += 1
     
+    price += edges * len(region)
+
+print(price)
