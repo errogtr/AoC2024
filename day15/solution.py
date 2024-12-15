ea7ed9f165994304fca454a3eee0ff973cb35eb3
enrico <enrico.dsf@gmail.com>
2024-12-15 18:32:35 +0100

    day15 p1 refactor

--- day15/solution.py
+++ day15/solution.py
@@ -1,9 +1,39 @@
+from copy import copy
+
+
 DIRS = {
     "^": complex(0, -1),
     ">": complex(1, 0),
     "<": complex(-1, 0),
     "v": complex(0, 1)
 }
+
+
+def move(curr_z, dirs, boxes, walls):
+    for dz in dirs:
+        next_z = curr_z + dz
+        
+        # do nothing if it hits a wall
+        if next_z in walls:
+            continue
+        
+        # move all boxes in same x/y direction if it hits a box
+        if next_z in boxes:
+            w = next_z
+            block = [next_z]
+            while w + dz in boxes:
+                w += dz
+                block.append(w)
+            
+            if block[-1] + dz in walls:
+                continue
+
+            for box in reversed(block):
+                boxes.remove(box)
+                boxes.add(box + dz)
+
+        curr_z = next_z
+    return boxes
 
 
 with open("day15/data") as f:
@@ -22,34 +52,6 @@ for y, row in enumerate(grid.splitlines()):
             curr_z = complex(x, y)
 
 
-pos = [curr_z]
-for dz in dirs:
-    next_z = curr_z + dz
-    
-    # do nothing if it hits a wall
-    if next_z in walls:
-        continue
-    
-    # move all boxes in same x/y direction if it hits a box
-    if next_z in boxes:
-        w = next_z
-        block = [next_z]
-        while w + dz in boxes:
-            w += dz
-            block.append(w)
-        
-        if block[-1] + dz in walls:
-            continue
-
-        for box in reversed(block):
-            boxes.remove(box)
-            boxes.add(box + dz)
-
-    curr_z = next_z
-    pos.append(curr_z)
-
-
-gps = 0
-for box in boxes:
-    gps += 100 * box.imag + box.real
-print(gps)
+# ==== PART 1 ====
+moved_boxes = move(curr_z, dirs, copy(boxes), copy(walls))
+print(sum(100 * box.imag + box.real for box in moved_boxes))
